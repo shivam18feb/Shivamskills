6bf26876cd456320192dc4328f021067c90d09bc
shivam pandey <48554579+shivam18feb@users.noreply.github.com>
2023-07-20 12:05:23 +0530

    Update settings.py
    remove mail keys

--- mywork/settings.py
+++ mywork/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shivam18feb@gmail.com'
-EMAIL_HOST_PASSWORD ='iuzsugnlvviwbcuy'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD =''
 EMAIL_USE_TLS = True 
 
 ALLOWED_HOSTS = []
